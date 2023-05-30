b6d2a8cc282e1e557215d7c260b94ead7c558bae
unknown <aniakoruc@gmail.com>
2023-05-30 13:06:50 +0200

    separate variable for API data

--- src/axios/getRecipeByName.ts
+++ src/axios/getRecipeByName.ts
@@ -1,16 +1,14 @@
+import { tastyApiVariables } from "@/utils/TastyApiVariables";
 import { axiosRequest } from "@/utils/axiosRequest";
 
 export const getRecipeByName = (search: string) => {
   const options = {
     method: "GET",
-    url: "https://tasty.p.rapidapi.com/recipes/auto-complete",
+    url: tastyApiVariables.url("auto-complete"),
     params: {
       prefix: search,
     },
-    headers: {
-      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_TASTY_API_KEY,
-      "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-    },
+    headers: tastyApiVariables.headers,
   };
 
   axiosRequest(options);
--- src/axios/getSimilarRecipes.ts
+++ src/axios/getSimilarRecipes.ts
@@ -1,14 +1,12 @@
+import { tastyApiVariables } from "@/utils/TastyApiVariables";
 import { axiosRequest } from "@/utils/axiosRequest";
 
 export const getSimilarRecipes = (id: string) => {
   const options = {
     method: "GET",
-    url: "https://tasty.p.rapidapi.com/recipes/list-similarities",
+    url: tastyApiVariables.url("list-similarities"),
     params: { recipe_id: id },
-    headers: {
-      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_TASTY_API_KEY,
-      "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-    },
+    headers: tastyApiVariables.headers,
   };
 
   axiosRequest(options);
