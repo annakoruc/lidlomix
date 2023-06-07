e069828490b680cc8bbb30fb0cd28c44b0509e33
unknown <aniakoruc@gmail.com>
2023-06-07 16:03:22 +0200

    change loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import LogoSvg from "@/assets/LogoSvg";
+// import { Footer } from "@/components";
 import React from "react";
+import "./pageStyle.scss";
+import { Rings } from "react-loader-spinner";
 
 const Loading = () => {
-  return <div>loading</div>;
+  return (
+    <main className="main_page">
+      <LogoSvg color="white" />
+      <Rings
+        height="80"
+        width="80"
+        color="#FFFFFF"
+        radius="6"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="rings-loading"
+      />
+    </main>
+  );
 };
 
 export default Loading;
