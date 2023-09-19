9f27ac5129900fb2988f30b9e7a2404b387832d7
Ania <aniakoruc@gmail.com>
2023-09-19 15:16:52 +0200

    add button to show some recipes - comment font in style all typography in project * warning in browsers console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,13 @@ export default function Home() {
   return (
     <BoxFlexComponent>
       <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
-        <NavigateButton href={"/login"} title="Login" variant="contained" />
-        <NavigateButton href={"/sign-up"} title="Sign Up" variant="outlined" />
+        <NavigateButton
+          href={"/all-recipes"}
+          title="Some recipes"
+          variant="contained"
+        />
+        {/* <NavigateButton href={"/login"} title="Login" variant="contained" />
+        <NavigateButton href={"/sign-up"} title="Sign Up" variant="outlined" /> */}
       </Box>
     </BoxFlexComponent>
   );
